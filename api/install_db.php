fc6f5aeb52209b2703380b92ff0f6463ab402c0b
Maciek Stepien <stepien.maciej@mail.com>
2018-05-31 22:01:41 +0200

    baza pelna

--- api/install_db.php
+++ api/install_db.php
@@ -35,6 +35,19 @@ if($conn->affected_rows < 1) {
   $conn->select_db($dbName);
   $updateDb = true;
   $raport .= 'Baza utworzona<br />';
+  $sql = 'DROP TABLE IF EXISTS `config`;';
+  $conn->query($sql);
+  $sql = 'CREATE TABLE `config` (
+    `KLUCZ` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+    `WARTOSC` varchar(150) COLLATE utf8_polish_ci NOT NULL,
+    `OPIS` text COLLATE utf8_polish_ci
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;';
+  $conn->query($sql);
+  $sql = 'ALTER TABLE `config` ADD PRIMARY KEY (`KLUCZ`);';
+  $conn->query($sql);
+  $sql = "INSERT INTO `config` (`KLUCZ`, `WARTOSC`, `OPIS`) VALUES ('DB_VER', '1', 'DB_VERSION');";
+  $conn->query($sql);
+  $raport .= 'Tabela konfiguracji utworzona<br />';
 } else {
   $conn->select_db($dbName);
   $sql = "SELECT `WARTOSC` FROM `CONFIG` WHERE `KLUCZ`='DB_VER' LIMIT 1;";
@@ -98,22 +111,55 @@ if($updateDb) {
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;';
         if($conn->query($sql)) {
           $raport .= 'Utworzono tabele zamowienia<br />';
-          $sql = "INSERT INTO `dostawcy` (`ID`, `NAZWA`, `ADRES`, `TELEFON`, `EMAIL`, `DODANO`) VALUES
-          (1, 'SAD MARIANA', 'ROWNIEZ SAD MARIANA', NULL, NULL, '2018-05-26 17:22:54');";
+          $sql = 'ALTER TABLE `dostawcy` ADD PRIMARY KEY (`ID`);';
           $conn->query($sql);
-          $sql = "INSERT INTO `klienci` (`ID`, `NAZWA`, `ADRES`, `TELEFON`, `EMAIL`) VALUES
-          (1, 'ZOSIA', 'DOM ZOSI', '608000000', NULL);";
+          $sql = 'ALTER TABLE `klienci` ADD PRIMARY KEY (`ID`);';
           $conn->query($sql);
-          $sql = "INSERT INTO `towary` (`ID`, `DOSTAWCAID`, `NAZWA`, `OPIS`, `ILOSC`) VALUES
-          (1, 1, 'JABLKA', 'swieze jabluszka dla wszystkich', 50.0000);";
+          $sql = 'ALTER TABLE `towary` ADD PRIMARY KEY (`ID`), ADD KEY `DOSTAWCAID` (`DOSTAWCAID`);';
           $conn->query($sql);
-          $sql = "INSERT INTO `zamowienia` (`ID`, `TOWAR`, `ILOSC`, `KLIENT`, `IDENTYFIKATOR`, `SPRZEDAZ`) VALUES
-          (1, 1, '2.0000', 1, 'japkazosi', '2018-05-26');";
+          $sql = 'ALTER TABLE `zamowienia`
+            ADD PRIMARY KEY (`ID`),
+            ADD UNIQUE KEY `UNIQUE_IDENTYFIKATOR` (`IDENTYFIKATOR`),
+            ADD KEY `TOWAR` (`TOWAR`),
+            ADD KEY `KLIENT` (`KLIENT`);';
+          $conn->query($sql);
+          $sql = 'ALTER TABLE `dostawcy`
+            MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;';
+          $conn->query($sql);
+          $sql = 'ALTER TABLE `klienci`
+            MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;';
+          $conn->query($sql);
+          $sql = 'ALTER TABLE `towary`
+            MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;';
+          $conn->query($sql);
+          $sql = 'ALTER TABLE `zamowienia`
+            MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;';
+          $conn->query($sql);
+          $sql = 'ALTER TABLE `towary`
+            ADD CONSTRAINT `towary_ibfk_1` FOREIGN KEY (`DOSTAWCAID`) REFERENCES `dostawcy` (`ID`);';
+          $conn->query($sql);
+          $sql = 'ALTER TABLE `zamowienia`
+            ADD CONSTRAINT `zamowienia_ibfk_1` FOREIGN KEY (`TOWAR`) REFERENCES `towary` (`ID`),
+            ADD CONSTRAINT `zamowienia_ibfk_2` FOREIGN KEY (`KLIENT`) REFERENCES `klienci` (`ID`);';
           if($conn->query($sql)) {
-            $raport .= 'Zasilono tabele poczatkowymi danymi<br />';
-            $sql = "UPDATE `CONFIG` SET `WARTOSC` = '".(string)$dbVersion."' WHERE `KLUCZ`='DB_VER';";
+            $raport .= 'Utworzono klucze dla tabel<br />';
+            $sql = "INSERT INTO `dostawcy` (`ID`, `NAZWA`, `ADRES`, `TELEFON`, `EMAIL`, `DODANO`) VALUES
+            (1, 'SAD MARIANA', 'ROWNIEZ SAD MARIANA', NULL, NULL, '2018-05-26 17:22:54');";
+            $conn->query($sql);
+            $sql = "INSERT INTO `klienci` (`ID`, `NAZWA`, `ADRES`, `TELEFON`, `EMAIL`) VALUES
+            (1, 'ZOSIA', 'DOM ZOSI', '608000000', NULL);";
+            $conn->query($sql);
+            $sql = "INSERT INTO `towary` (`ID`, `DOSTAWCAID`, `NAZWA`, `OPIS`, `ILOSC`) VALUES
+            (1, 1, 'JABLKA', 'swieze jabluszka dla wszystkich', 50.0000);";
+            $conn->query($sql);
+            $sql = "INSERT INTO `zamowienia` (`ID`, `TOWAR`, `ILOSC`, `KLIENT`, `IDENTYFIKATOR`, `SPRZEDAZ`) VALUES
+            (1, 1, '2.0000', 1, 'japkazosi', '2018-05-26');";
             if($conn->query($sql)) {
-              $raport .= 'Podniesiono wersje bazy do aktualnej( '.(string)$dbVersion.' )<br />';
+              $raport .= 'Zasilono tabele poczatkowymi danymi<br />';
+              $sql = "UPDATE `CONFIG` SET `WARTOSC` = '".(string)$dbVersion."' WHERE `KLUCZ`='DB_VER';";
+              if($conn->query($sql)) {
+                $raport .= 'Podniesiono wersje bazy do aktualnej( '.(string)$dbVersion.' )<br />';
+              }
             }
           }
         }
